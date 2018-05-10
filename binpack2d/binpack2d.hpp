07cb27c617d422f988af6092fd94c803966b1cb4
tamasmeszaros <meszaros.q@gmail.com>
2018-05-10 12:21:00 +0200

    Lot of comments and removed the static items vector form Arranger which was a pretty ugly solution.

--- binpack2d/binpack2d.hpp
+++ binpack2d/binpack2d.hpp
@@ -13,7 +13,14 @@
 namespace binpack2d {
 
 /**
- * An item to be placed on a bin.
+ * \brief An item to be placed on a bin.
+ *
+ * It holds a copy of the original shape object but supports move construction
+ * from the shape objects if its an rvalue reference. This way we can construct
+ * the items without the cost of copying a potentially large amount of input.
+ *
+ * The results of some calculations are cached for maintaining fast run times.
+ * For this reason, memory demands are much higher but this should pay off.
  */
 template<class RawShape>
 class _Item {
@@ -29,7 +36,7 @@ class _Item {
     Radians rotation_;
     Coord offset_distance_;
 
-    // Info about wheter the tranformations will have to take place
+    // Info about whether the tranformations will have to take place
     // This is needed because if floating point is used, it is hard to say
     // that a zero angle is not a rotation because of testing for equality.
     bool has_rotation_ = false, has_translation_ = false, has_offset_ = false;
@@ -43,50 +50,106 @@ class _Item {
     mutable bool offset_cache_valid_ = false;
 public:
 
+    /// The type of the shape which was handed over as the template argument.
     using ShapeType = RawShape;
+
+    /**
+     * \brief Iterator type for the outer vertices.
+     *
+     * Only const iterators can be used. The _Item type is not intended to
+     * modify the carried shapes from the outside. The main purpose of this type
+     * is to cache the calculation results from the various operators it
+     * supports. Giving out a non const iterator would make it impossible to
+     * perform correct cache invalidation.
+     */
     using Iterator = TVertexConstIterator<RawShape>;
 
+    /**
+     * @brief Get the orientation of the polygon.
+     *
+     * The orientation have to be specified as a specialization of the
+     * OrientationType struct which has a Value constant.
+     *
+     * @return The orientation type identifier for the _Item type.
+     */
     static BP2D_CONSTEXPR Orientation orientation() {
         return OrientationType<RawShape>::Value;
     }
 
+    /**
+     * @brief Constructing an _Item form an existing raw shape. The shape will
+     * be copied into the _Item object.
+     * @param sh The original shape object.
+     */
     explicit inline _Item(const RawShape& sh): sh_(sh) {}
 
+    /**
+     * @brief Construction of an item by moving the content of the raw shape,
+     * assuming that it supports move semantics.
+     * @param sh The original shape object.
+     */
     explicit inline _Item(RawShape&& sh): sh_(std::move(sh)) {}
 
+    /**
+     * @brief Create an item from an initilizer list.
+     * @param il The initializer list of vertices.
+     */
     inline _Item(const std::initializer_list< Vertex >& il):
         sh_(ShapeLike::create<RawShape>(il)) {}
 
+    /**
+     * @brief Convert the polygon to string representation. The format depends
+     * on the implementation of the polygon.
+     * @return
+     */
     inline std::string toString() const
     {
         return ShapeLike::toString(sh_);
     }
 
+    /// Iterator tho the first vertex in the polygon.
     inline Iterator begin() const
     {
         return ShapeLike::cbegin(sh_);
     }
 
+    /// Alias to begin()
     inline Iterator cbegin() const
     {
         return ShapeLike::cbegin(sh_);
     }
 
+    /// Iterator to the last element.
     inline Iterator end() const
     {
         return ShapeLike::cend(sh_);
     }
 
+    /// Alias to end()
     inline Iterator cend() const
     {
         return ShapeLike::cend(sh_);
     }
 
+    /**
+     * @brief Get o copy of an outer vertex whithin the carried shape.
+     * @param idx The index of the requested vertex.
+     * @return A copy of the requested vertex.
+     */
     inline Vertex vertex(unsigned long idx) const
     {
         return ShapeLike::vertex(sh_, idx);
     }
 
+    /**
+     * @brief Modify a vertex.
+     *
+     * Note that this method will invalidate every cached calculation result
+     * including polygon offset and transformation.
+     *
+     * @param idx The index of the requested vertex.
+     * @param v The new vertex data.
+     */
     inline void setVertex(unsigned long idx,
                           const Vertex& v )
     {
@@ -94,6 +157,14 @@ public:
         ShapeLike::vertex(sh_, idx) = v;
     }
 
+    /**
+     * @brief Calculate the shape area.
+     *
+     * The method returns absolute value and does not reflect polygon
+     * orientation. The result is cached, subsequent calls will have very little
+     * cost.
+     * @return The shape area in floating point double precision.
+     */
     inline double area() const {
         double ret ;
         if(area_cache_valid_) ret = area_cache_;
@@ -105,10 +176,16 @@ public:
         return ret;
     }
 
+    /// The number of the outer ring vertices.
     inline unsigned long vertexCount() const {
         return ShapeLike::contourVertexCount(sh_);
     }
 
+    /**
+     * @brief isPointInside
+     * @param p
+     * @return
+     */
     inline bool isPointInside(const Vertex& p)
     {
         return ShapeLike::isInside(p, sh_);
@@ -237,7 +314,7 @@ private:
 };
 
 /**
- * Subclass of _Item for regular rectangle items.
+ * \brief Subclass of _Item for regular rectangle items.
  */
 template<class RawShape>
 class _Rectangle: public _Item<RawShape> {
@@ -286,21 +363,47 @@ public:
 };
 
 /**
- * A wrapper interface (trait) class for any placement strategy provider.
+ * \brief A wrapper interface (trait) class for any placement strategy provider.
+ *
+ * If a client want's to use its own placement algorithm, all it has to do is to
+ * specialize this class template and define all the ten methods it has. It can
+ * use the strategies::PlacerBoilerplace class for creating a new placement
+ * strategy where only the constructor and the trypack method has to be provided
+ * and it will work out of the box.
  */
 template<class PlacementStrategy>
 class PlacementStrategyLike {
     PlacementStrategy impl_;
 public:
+
+    /// The item type that the placer works with.
     using Item = typename PlacementStrategy::Item;
+
+    /// The placer's config type. Should be a simple struct but can be anything.
     using Config = typename PlacementStrategy::Config;
+
+    /**
+     * \brief The type of the bin that the placer works with.
+     *
+     * Can be a box or an arbitrary shape or just a width or height without a
+     * second dimension if an infinite bin is considered.
+     */
     using BinType = typename PlacementStrategy::BinType;
+
+    /**
+     * \brief Pack result that can be used to accept or discard it. See trypack
+     * method.
+     */
+    using PackResult = typename PlacementStrategy::PackResult;
 
     using ItemRef = std::reference_wrapper<Item>;
     using ItemGroup = std::vector<ItemRef>;
 
-    using PackResult = typename PlacementStrategy::PackResult;
-
+    /**
+     * @brief Constructor taking the bin and an optional configuration.
+     * @param bin The bin object whose type is defined by the placement strategy.
+     * @param config The configuration for the particular placer.
+     */
     explicit PlacementStrategyLike(const BinType& bin,
                                    const Config& config = Config()):
         impl_(bin)
@@ -308,26 +411,72 @@ public:
         configure(config);
     }
 
+    /**
+     * @brief Provide a different configuration for the placer.
+     *
+     * Note that it depends on the particular placer implementation how it
+     * reacts to config changes in the middle of a calculation.
+     *
+     * @param config The configuration object defined by the placement startegy.
+     */
     inline void configure(const Config& config) { impl_.configure(config); }
 
+    /**
+     * @brief A method that tries to pack an item and returns an object
+     * describing the pack result.
+     *
+     * The result can be casted to bool and used as an argument to the accept
+     * method to accept a succesfully packed item. This way the next packing
+     * will consider the accepted item as well. The PackResult should carry the
+     * transformation info so that if the tried item is later modified or tried
+     * multiple times, the result object should set it to the originally
+     * determied position. An implementation can be found in the
+     * strategies::PlacerBoilerplate::PackResult class.
+     *
+     * @param item Ithe item to be packed.
+     * @return The PackResult object that can be implicitly casted to bool.
+     */
     inline PackResult trypack(Item& item) { return impl_.trypack(item); }
 
+    /**
+     * @brief A method to accept a previously tried item.
+     *
+     * If the pack result is a failure the method should ignore it.
+     * @param r The result of a previous trypack call.
+     */
     inline void accept(PackResult& r) { impl_.accept(r); }
 
+    /**
+     * @brief pack Try to pack an item and immediately accept it on success.
+     *
+     * A default implementation would be to call
+     * { auto&& r = trypack(item); accept(r); return r; } but we should let the
+     * implementor of the placement strategy to harvest any optimizations from
+     * the absence of an intermadiate step. The above version can still be used
+     * in the implementation.
+     *
+     * @param item The item to pack.
+     * @return Returns true if the item was packed or false if it could not be
+     * packed.
+     */
     inline bool pack(Item& item) { return impl_.pack(item); }
 
+    /// Unpack the last element (remove it from the list of packed items).
     inline void unpackLast() { impl_.unpackLast(); }
 
+    /// Get the bin object.
     inline const BinType& bin() const { return impl_.bin(); }
 
+    /// Set a new bin object.
     inline void bin(const BinType& bin) { impl_.bin(bin); }
 
+    /// Get the packed items.
     inline ItemGroup getItems() { return impl_.getItems(); }
 
+    /// Clear the packed items so a new session can be started.
     inline void clearItems() { impl_.clearItems(); }
 
 };
-
 
 /**
  * A wrapper interface (trait) class for any selections strategy provider.
@@ -342,10 +491,30 @@ public:
     using ItemRef = std::reference_wrapper<Item>;
     using ItemGroup = std::vector<ItemRef>;
 
+    /**
+     * @brief Provide a different configuration for the selection strategy.
+     *
+     * Note that it depends on the particular placer implementation how it
+     * reacts to config changes in the middle of a calculation.
+     *
+     * @param config The configuration object defined by the selection startegy.
+     */
     inline void configure(const Config& config) {
         impl_.configure(config);
     }
 
+    /**
+     * \brief A method to start the calculation on the input sequence.
+     *
+     * \tparam TPlacer The only mandatory template parameter is the type of
+     * placer compatible with the PlacementStrategyLike interface.
+     *
+     * \param first, last The first and last iterator if the input sequence. It
+     * can be only an iterator of a type converitible to Item.
+     * \param bin. The shape of the bin. It has to be supported by the placement
+     * strategy.
+     * \param An optional config object for the placer.
+     */
     template<class TPlacer, class TIterator,
              class TBin = typename PlacementStrategyLike<TPlacer>::BinType,
              class PConfig = typename PlacementStrategyLike<TPlacer>::Config>
@@ -360,17 +529,35 @@ public:
                                  std::forward<PConfig>(config));
     }
 
+    /**
+     * \brief Get the number of bins opened by the selection algorithm.
+     *
+     * Initially it is zero and after the call to packItems it will return
+     * the number of bins opened by the packing procedure.
+     *
+     * \return The number of bins opened.
+     */
     inline size_t binCount() const { return impl_.binCount(); }
 
+    /**
+     * @brief Get the items for a particular bin.
+     * @param binIndex The index of the requested bin.
+     * @return Returns a list of allitems packed into the requested bin.
+     */
     inline ItemGroup itemsForBin(size_t binIndex) {
         return impl_.itemsForBin(binIndex);
     }
 
+    /// Same as itemsForBin but for a const context.
     inline const ItemGroup itemsForBin(size_t binIndex) const {
         return impl_.itemsForBin(binIndex);
     }
 };
 
+
+/**
+ * \brief A list of packed item vectors. Each vector represents a bin.
+ */
 template<class RawShape>
 using _PackGroup = std::vector<
                         std::vector<
@@ -378,6 +565,14 @@ using _PackGroup = std::vector<
                         >
                    >;
 
+/**
+ * \brief A list of packed (index, item) pair vectors. Each vector represents a
+ * bin.
+ *
+ * The index is points to the position of the item in the original input
+ * sequence. This way the caller can use the items as a transformation data
+ * carrier and transform the original objects manually.
+ */
 template<class RawShape>
 using _IndexedPackGroup = std::vector<
                                std::vector<
@@ -408,8 +603,6 @@ public:
 
     using Unit = TCoord<typename Item::ShapeType>;
 
-    // PackGroup is a table where cells are pairs of an ItemRef with its
-    // appropriate index in the input
     using IndexedPackGroup = _IndexedPackGroup<typename Item::ShapeType>;
     using PackGroup = _PackGroup<typename Item::ShapeType>;
 
@@ -422,12 +615,20 @@ private:
     using TPItem = remove_cvref_t<Item>;
     using TSItem = remove_cvref_t<SItem>;
 
+    std::vector<TPItem> item_cache_;
+
 public:
 
+    /**
+     * \brief Constructor taking the bin as the only mandatory parameter.
+     *
+     * \param bin The bin shape that will be used by the placers. The type
+     * of the bin should be one that is supported by the placer type.
+     */
     template<class TBinType = BinType,
              class PConf = PlacementConfig,
              class SConf = SelectionConfig>
-    Arranger(TBinType&& bin,
+    Arranger( TBinType&& bin,
               Unit min_obj_distance = 0,
               PConf&& pconfig = PConf(),
               SConf&& sconfig = SConf()):
@@ -441,18 +642,33 @@ public:
         selector_.configure(std::forward<SelectionConfig>(sconfig));
     }
 
+    /**
+     * \brief Arrange an input sequence and return a PackGroup object with
+     * the packed groups corresponding to the bins.
+     *
+     * The number of groups in the pack group is the number of bins opened by
+     * the selection algorithm.
+     */
     template<class TIterator>
     inline PackGroup arrange(TIterator from, TIterator to)
     {
         return _arrange(from, to);
     }
 
+    /**
+     * A version of the arrange method returning an IndexedPackGroup with
+     * the item indexes into the original input sequence.
+     *
+     * Takes a little longer to collect the indices. Scales linearly with the
+     * input sequence size.
+     */
     template<class TIterator>
     inline IndexedPackGroup arrangeIndexed(TIterator from, TIterator to)
     {
         return _arrangeIndexed(from, to);
     }
 
+    /// Shorthand to normal arrange method.
     template<class TIterator>
     inline PackGroup operator() (TIterator from, TIterator to)
     {
@@ -489,10 +705,9 @@ private:
              >
     inline PackGroup _arrange(TIterator from, TIterator to, int = false)
     {
-        static std::vector<TPItem> items;
-        items = {from, to};
+        item_cache_ = {from, to};
 
-        __arrange(items.begin(), items.end());
+        __arrange(item_cache_.begin(), item_cache_.end());
 
         PackGroup ret;
         for(size_t i = 0; i < selector_.binCount(); i++) {
@@ -528,9 +743,8 @@ private:
                                             TIterator to,
                                             int = false)
     {
-        static std::vector<TPItem> items;
-        items = {from, to};
-        __arrange(items.begin(), items.end());
+        item_cache_ = {from, to};
+        __arrange(item_cache_.begin(), item_cache_.end());
         return createIndexedPackGroup(from, to, selector_);
     }
 

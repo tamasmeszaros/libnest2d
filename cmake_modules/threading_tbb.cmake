7f9967186921f81ce5d10cd393b4940faf1affea
tamasmeszaros <meszaros.q@gmail.com>
2019-11-07 23:36:36 +0100

    Switch to tbb on clang build

--- cmake_modules/threading_tbb.cmake
+++ cmake_modules/threading_tbb.cmake
@@ -7,7 +7,7 @@ endif()
 require_package(TBB REQUIRED)
 
 target_link_libraries(tbbThreading INTERFACE TBB::tbb)
-target_compile_definitions(tbbThreading INTERFACE -DTBB_USE_CAPTURED_EXCEPTION=0)
+target_compile_definitions(tbbThreading INTERFACE -DTBB_USE_CAPTURED_EXCEPTION)
 
 install(TARGETS tbbThreading EXPORT Libnest2DTargets INCLUDES DESTINATION include)
 set(LIBNEST2D_PUBLIC_PACKAGES "${LIBNEST2D_PUBLIC_PACKAGES};TBB" CACHE INTERNAL "")

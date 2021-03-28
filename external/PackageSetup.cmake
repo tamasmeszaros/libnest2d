f807ce132404624651aa8d62bd45be8a6966ded8
tamasmeszaros <meszaros.q@gmail.com>
2021-03-28 15:49:57 +0200

    Remove leftover junk

--- external/PackageSetup.cmake
+++ external/PackageSetup.cmake
@@ -2,9 +2,6 @@ set(RP_INSTALL_PREFIX "${CMAKE_CURRENT_BINARY_DIR}/destdir/" CACHE STRING "")
 
 set(RP_Boost_VERSION 1.70)
 
-set (RP_OpenVDB_COMPONENTS openvdb)
-set (RP_wxWidgets_VERSION 3.1.3)
-
 if (NOT BUILD_SHARED_LIBS)
     set(TBB_STATIC ON)
 endif ()
